ea205c7cf52a9ad3057fa0000b37fca4c0c0cb54
nsikakgoldman <georgegoldman.john.o.245299@unn.edu.ng>
2023-10-03 14:04:11 +0100

    fixed

---
+++ Functions.cpp
@@ -0,0 +1,12 @@
+//
+// Created by golman on 10/10/23.
+//
+double add(double x, double y)
+{
+    return x + y;
+}
+
+double add(double a, double b, double c)
+{
+    return a + b + c;
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+
+#include "Functions.h"
+
+int main() {
+    int total = add(3, 4);
+    cout << "3 + 4 is "<< total << '\n';
+
+    auto totalofthree = add(1.1, 2.2, 3.3);
+    cout << "1.1 + 2.2 + 3.3 = "<< totalofthree << '\n';
+    return 0;
+}
