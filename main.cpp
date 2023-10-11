4f5b76f9765b26cd9dbf5769c07d515b401af582
nsikakgoldman <georgegoldman.john.o.245299@unn.edu.ng>
2023-10-11 16:58:41 +0100

    updt

--- main.cpp
+++ main.cpp
@@ -19,7 +19,44 @@ using std::count;
 
 #include "Account.h"
 
+//function pointers
+#include <cstdio>
+
+int addition(int a, int b) {return (a + b);}
+int subtraction(int a, int b) {return (a - b);}
+typedef int (*operation)(int a, int b);
+#define ARRAYSIZE 5
+
+#define SQ(a) (a)*(a)
+
 int main() {
+// function pointers
+    int numbers[ARRAYSIZE] = {200, 600, 123, 456, 789};
+    int twentyfive = SQ(ARRAYSIZE);
+
+    int total = 0;
+    operation op = addition;
+    for (int n = 0; n < ARRAYSIZE; n++)
+    {
+        total = op(total, numbers[n]);
+    }
+    printf("Array total: %d \n", total);
+    printf("\n\n");
+
+    op = subtraction;
+    total = op(total, numbers[ARRAYSIZE - 1]);
+    printf("Array total without last element: %d \n", total);
+    printf("\n\n");
+
+    const char* greeting = "Hello";
+    printf("string: %s", greeting);
+    printf("\n\n");
+    printf("character: %c", greeting[0]);
+    printf("\n\n");
+    printf("pointer: %p", greeting);
+    printf("\n\n");
+//    printf("hex: 0x%08x", greeting);
+    printf("\n\n");
 //    int total = add(3, 4);
 //    cout << "3 + 4 is "<< total << '\n';
 
@@ -85,25 +122,25 @@ int main() {
     // operator overloading
 //    += == << >> ::
 //classes
-    Account a1;
-    a1.Deposite(90);
-    cout << "After depositing $90 " <<
-    "balance is $"<< a1.GetBalance() << '\n';
-    for (auto s : a1.Report())
-    {
-        cout << s << '\n';
-    }
-
-    a1.Withdraw(50);
-    if (a1.Withdraw(100))
-    {
-        cout << "Second withdraw succeeds" << '\n';
-    }
-    cout << "After withdrawing $50 then $100" << '\n';
-    for (auto s : a1.Report())
-    {
-        cout << s << '\n';
-    }
+//    Account a1;
+//    a1.Deposite(90);
+//    cout << "After depositing $90 " <<
+//    "balance is $"<< a1.GetBalance() << '\n';
+//    for (auto s : a1.Report())
+//    {
+//        cout << s << '\n';
+//    }
+//
+//    a1.Withdraw(50);
+//    if (a1.Withdraw(100))
+//    {
+//        cout << "Second withdraw succeeds" << '\n';
+//    }
+//    cout << "After withdrawing $50 then $100" << '\n';
+//    for (auto s : a1.Report())
+//    {
+//        cout << s << '\n';
+//    }
     return 0;
 
 }
