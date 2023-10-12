58ebcb66ac85039a2e97212f4ce195d7a3ebaea7
nsikakgoldman <georgegoldman.john.o.245299@unn.edu.ng>
2023-10-12 04:17:07 +0100

    test the implemwentation of pointers

--- main.cpp
+++ main.cpp
@@ -22,41 +22,41 @@ using std::count;
 //function pointers
 #include <cstdio>
 
-int addition(int a, int b) {return (a + b);}
-int subtraction(int a, int b) {return (a - b);}
+//int addition(int a, int b) {return (a + b);}
+//int subtraction(int a, int b) {return (a - b);}
 typedef int (*operation)(int a, int b);
-#define ARRAYSIZE 5
-
-#define SQ(a) (a)*(a)
+//#define ARRAYSIZE 5
+//
+//#define SQ(a) (a)*(a)
 
 int main() {
 // function pointers
-    int numbers[ARRAYSIZE] = {200, 600, 123, 456, 789};
-    int twentyfive = SQ(ARRAYSIZE);
-
-    int total = 0;
-    operation op = addition;
-    for (int n = 0; n < ARRAYSIZE; n++)
-    {
-        total = op(total, numbers[n]);
-    }
-    printf("Array total: %d \n", total);
-    printf("\n\n");
-
-    op = subtraction;
-    total = op(total, numbers[ARRAYSIZE - 1]);
-    printf("Array total without last element: %d \n", total);
-    printf("\n\n");
-
-    const char* greeting = "Hello";
-    printf("string: %s", greeting);
-    printf("\n\n");
-    printf("character: %c", greeting[0]);
-    printf("\n\n");
-    printf("pointer: %p", greeting);
-    printf("\n\n");
-//    printf("hex: 0x%08x", greeting);
-    printf("\n\n");
+//    int numbers[ARRAYSIZE] = {200, 600, 123, 456, 789};
+//    int twentyfive = SQ(ARRAYSIZE);
+//
+//    int total = 0;
+//    operation op = addition;
+//    for (int n = 0; n < ARRAYSIZE; n++)
+//    {
+//        total = op(total, numbers[n]);
+//    }
+//    printf("Array total: %d \n", total);
+//    printf("\n\n");
+//
+//    op = subtraction;
+//    total = op(total, numbers[ARRAYSIZE - 1]);
+//    printf("Array total without last element: %d \n", total);
+//    printf("\n\n");
+//
+//    const char* greeting = "Hello";
+//    printf("string: %s", greeting);
+//    printf("\n\n");
+//    printf("character: %c", greeting[0]);
+//    printf("\n\n");
+//    printf("pointer: %p", greeting);
+//    printf("\n\n");
+//    printf("hex: 0x08%x\n", greeting);
+//    printf("\n\n");
 //    int total = add(3, 4);
 //    cout << "3 + 4 is "<< total << '\n';
 
@@ -141,6 +141,17 @@ int main() {
 //    {
 //        cout << s << '\n';
 //    }
+//    pointer
+    int a = 3;
+    printf("a is %i \n", a);
+    int& rA = a;
+    rA = 5;
+    printf("a is %i \n", a);
+
+//    Person Kate("Georgegoldman", "John", 234);
+//    Person& rKate = Kate;
+//    rKate.SetNumber(345);
+//    printf("rKate: %s %i", rKate.GetName(), rKate.GetNumber())
     return 0;
 
 }
